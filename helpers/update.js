00d87009868ef1b8301dc6ae973030f2e5208a02
quelhasu <uquelhas@gmail.com>
2019-05-07 09:43:36 +0200

    :bulb: global functions

--- helpers/update.js
+++ helpers/update.js
@@ -1,3 +1,9 @@
+/**
+ * Formats age for query process
+ * @param {String} age - String to modify
+ * 
+ * @return {String} formated string 
+ */
 exports.ages = (age) => {
   if(age == "all" || age == null || age == "-")
     return "";
@@ -5,10 +11,22 @@ exports.ages = (age) => {
     return " and a1.age = '"+age+"'"
 };
 
+/**
+ * Formats region for query process
+ * @param {String} region - String to modify
+ * 
+ * @return {String} formated string 
+ */
 exports.region = (region) => {
   return ' a0.name_1 = \''+region+'\' AND a2.name_1 = \''+region+'\' '
 };
 
+/**
+ * Create differential array to compare ingoing/outgoig values over years
+ * @param {String} region - String to modify
+ * 
+ * @return {String} formated string 
+ */
 exports.diffGoing = (array) => {
   for (var region in array) {
     for (var year in array[region]) {
@@ -24,10 +42,22 @@ exports.diffGoing = (array) => {
   return array;
 };
 
+/**
+ * Format number to process query
+ * @param {Number} num - String to modify
+ * 
+ * @return {String} formated string 
+ */
 exports.formatNumber = (num) => {
   return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ')
 }
 
+/**
+ * Create differential array to compare values over years
+ * @param {String} region - String to modify
+ * 
+ * @return {String} formated string 
+ */
 exports.diff = (array) => {
   for (var country in array) {
     for (var year in array[country]) {
@@ -41,6 +71,12 @@ exports.diff = (array) => {
   return array;
 }
 
+/**
+ * Get evolution over two years (Y/Y-1)
+ * @param {String} region - String to modify
+ * 
+ * @return {String} formated string 
+ */
 exports.percentDiff = (oldV, newV, nbVal = [1]) => {
   let obj = {};
   nbVal.forEach(i => {
@@ -49,15 +85,37 @@ exports.percentDiff = (oldV, newV, nbVal = [1]) => {
   return obj;
 }
 
+/**
+ * Round decimal according a given number of decimal
+ * @param {Number} nbDec - Number of decimal
+ * 
+ * @class Number
+ * @function roundDecimal
+ * @return {Number} formated decimal 
+ */
 Number.prototype.roundDecimal = function (nbDec) {
   // +(val) => string converted to a Number
   return +(Math.round(this + "e+" + nbDec)  + "e-" + nbDec);
 };
 
+/**
+ * Capitalize a given string
+ * 
+ * @class String
+ * @function capitalize
+ * @return {String} formated string 
+ */
 String.prototype.capitalize = function () {
   return this.charAt(0).toUpperCase() + this.slice(1);
 };
 
+/**
+ * Formats string for process query
+ * 
+ * @class String
+ * @function nameQuery
+ * @return {String} formated string 
+ */
 String.prototype.nameQuery = function () {
   return "name_"+this;
 };
